fa19f557b1acf9da1a8de239352f686b73a6de97
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-01 15:55:21 +0530

    new files

---
+++ bootstrap/learning bootstrap/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+
+function App() {
+
+  return <div>
+    this is my first boostrap
+    <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-danger">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+
+  </div>
+  
+    
+      
+       
+}
+
+export default App
