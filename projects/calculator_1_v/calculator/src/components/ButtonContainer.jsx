5d72e8487ccfd8c0c80194f82c4ca775621d9288
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-07 23:47:47 +0530

    calculator

---
+++ projects/calculator_1_v/calculator/src/components/ButtonContainer.jsx
@@ -0,0 +1,11 @@
+import styles from "./ButtonContainer.module.css";
+const ButtonContainer = () => {
+  const buttonNames=["c",'1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.'];
+  return <>
+   <div className={styles.buttonContainer} >
+    {buttonNames.map(buttonNames => <button className={styles.button}>{buttonNames}</button>)} 
+        
+      </div>
+  </>
+}
+export default ButtonContainer;
