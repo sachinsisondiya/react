023e9af2462d958357d9e82e93c5655abd7fa00a
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-10 22:01:45 +0530

    improve function

--- projects/todo-app-project_3/src/App.jsx
+++ projects/todo-app-project_3/src/App.jsx
@@ -4,23 +4,11 @@ import Addtodo from "./component/Addtodo";
 import Todoitemss from "./component/Todoitemss";
 import "./App.css";
 import { useState } from "react";
+import WelcomeMessage from "./component/WelcomeMessage";
 function App() {
-  const initialTodoItems = [
-    {
-      name: "Buy Milk",
-      dueDate: "4/10/2025",
-    },
-    {
-      name: "Go to College",
-      dueDate: "4/10/2025",
-    },
-     {
-      name: "Like this video",
-      dueDate: "right now",
-    },
-  ];
+  
 
-  let [todoItems,setTodoItems]= useState(initialTodoItems);
+  let [todoItems,setTodoItems]= useState([]);
 
   const handleNewItems=(itemName,itemDate)=>{
     console.log(`New Item Added: ${itemName}
@@ -37,6 +25,7 @@ function App() {
       <AppName></AppName>
       <Addtodo onNewItem={handleNewItems}></Addtodo>
       <Todoitemss todoItems={todoItems}></Todoitemss>
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
       
     </div>
   );
---
+++ projects/todo-app-project_3/src/component/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+import style from "./WelcomeMessage.module.css";
+const WelcomeMessage = () => {
+return <>
+  <p className={style.welcomeModule}> Enjoy your day</p>
+
+  
+   </>
+}
+export default WelcomeMessage;
