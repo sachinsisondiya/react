6cd14b7f0c767b8c52e45c897e8bd5f8a81bf21e
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-15 01:07:02 +0530

    clock projects

--- projects/bharats-clock-vesrion/src/container/time.jsx
+++ projects/bharats-clock-vesrion/src/container/time.jsx
@@ -1,5 +1,17 @@
+import { useState, useEffect } from "react";
+
 function Time(){
-  let time= new Date();
+  const [time,setTime]=useState(new Date());
+
+  useEffect(()=>{
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    },1000);
+
+    return () =>{
+      clearInterval(intervalId);
+    }
+  }, []);
   return(
      <p>this is the current time: {time.toLocaleDateString()}-{time.toLocaleTimeString()}</p>
   )
